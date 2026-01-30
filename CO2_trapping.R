d623b07b512f49fd308e16607d8c1942b1177469
ViktorGaardman <viktorg97@live.se>
2026-01-30 17:22:31 -0200

    Cleaning Co2 data

--- CO2_trapping.R
+++ CO2_trapping.R
@@ -6,6 +6,9 @@ library(emmeans)
 library(performance)
 library(lubridate)
 
+
+rm(list=ls())
+
 df <- read.csv("TrappingCombinations.csv", sep =";", h = T)
 
 #Sampled individuals per hour
@@ -148,29 +151,144 @@ CO2_data <- map_dfr(files, read_logger_file)
 
 CO2_data$CO2 <- as.numeric(CO2_data$CO2)
 
-CO2_filtered <- CO2_data %>%
-  filter(!is.na(CO2)) %>%
-  filter(CO2 >= 600, CO2 <= 2010)
+CO2_data <- CO2_data %>%
+  filter(!is.na(CO2)) 
 
-write.csv(CO2_filtered, "CO2_data_cleaned.csv")
-
-#Calculate ml CO2 per minute
-#First per second, then times 60
-
-#Get time in seconds
-CO2_filtered <- CO2_filtered %>%
+CO2_data <- CO2_data %>%
   mutate(TAIR = as.numeric(TAIR)) %>%
   mutate(AIRPRES = as.numeric(`AIR PRESSURE`)) %>%
   mutate(
     EventSec = period_to_seconds(hms(`EVENT TIME`))
   )
 
+CO2_data <- CO2_data %>%
+  mutate(
+    Hour = str_extract(SourceFile, "(?<=_)[0-9]+(?=\\.txt)")
+  ) %>%
+  mutate(Hour = as.numeric(Hour))  # optional, makes it numeric
+
+CO2_data$Hour <- as.factor(CO2_data$Hour)
+CO2_data$SourceFile <- as.factor(CO2_data$SourceFile)
+
+
+CO2_data$`EVENT DATE` <- as.factor(CO2_data$`EVENT DATE`)
+levels(CO2_data$`EVENT DATE`)
+#%>%
+#  filter(CO2 >= 600, CO2 <= 2010)
+
+write.csv(CO2_data, "CO2_data_cleaned.csv")
+
+CO2_data <- read_csv("CO2_data_cleaned.csv")
+
+ggplot(subset(CO2_data, SourceFile == "bag1_10.txt"),
+       aes(x = EventSec, y = CO2)) +
+  geom_point()+
+  geom_smooth()+
+facet_wrap(~ `EVENT DATE`, scales = "free_x")
+
+ggplot(subset(CO2_filtered, `EVENT DATE` == "10/06/25"),
+       aes(x = EventSec, y = CO2)) +
+  geom_point()+
+  geom_smooth()+
+  facet_wrap(~ SourceFile, scales = "free_x")
+
+#Remove wonky measurements:
+CO2_cleaned <- CO2_data %>%
+  filter(!(`EVENT DATE` == "13/05/25" & SourceFile %in% c("bag1_9.txt",
+                                                        "bag2_9.txt",
+                                                        "bag3_13.txt",
+                                                        "bag2_15.txt",
+                                                        "bag1_16.txt",
+                                                        "bag2_19.txt")))%>%
+  filter(!(`EVENT DATE` == "25/05/25" & SourceFile %in% c("bag2_10.txt",
+                                                          "bag3_11.txt",
+                                                        "bag1_12.txt",
+                                                        "bag2_12.txt",
+                                                        "bag3_12.txt",
+                                                        "bag2_13.txt",
+                                                        "bag3_13.txt",
+                                                        "bag3_14.txt",
+                                                        "bag1_15.txt",
+                                                        "bag2_15.txt",
+                                                        "bag3_15.txt",
+                                                        "bag1_16.txt",
+                                                        "bag3_16.txt",
+                                                        "bag2_17.txt",
+                                                        "bag3_17.txt",
+                                                        "bag1_18.txt",
+                                                        "bag2_18.txt",
+                                                        "bag3_18.txt",
+                                                        "bag1_20.txt",
+                                                        "bag1_22.txt",
+                                                        "bag2_22.txt",
+                                                        "bag3_22.txt")))%>%
+  filter(!(`EVENT DATE` == "10/06/25" & SourceFile %in% c("bag2_10.txt",
+                                                        "bag1_14.txt",
+                                                        "bag2_15.txt",
+                                                        "bag1_16.txt",
+                                                        "bag3_18.txt")))
+
+
+
+ggplot(subset(CO2_cleaned, SourceFile == "bag3_16.txt"),
+       aes(x = EventSec, y = CO2)) +
+  geom_point()+
+  geom_smooth()+
+  facet_wrap(~ `EVENT DATE`, scales = "free_x")
+
+#Trim some measurements to start at the right time:
+CO2_cleaned <- CO2_cleaned %>%
+  filter(
+    !(`EVENT DATE` == "10/06/25" & SourceFile == "bag1_12.txt") |
+      EventSec >= 36690
+  )
+
+CO2_cleaned <- CO2_cleaned %>%
+  filter(
+    !(`EVENT DATE` == "13/05/25" & SourceFile == "bag2_16.txt") |
+      EventSec >= 50306
+  )
+
+CO2_cleaned <- CO2_cleaned %>%
+  filter(
+    !(`EVENT DATE` == "25/05/25" & SourceFile == "bag3_20.txt") |
+      EventSec >= 65275
+  )
+
+CO2_cleaned <- CO2_cleaned %>%
+  filter(
+    !(`EVENT DATE` == "25/05/25" & SourceFile == "bag3_11.txt") |
+      EventSec >= 32030
+  )
+
+write.csv(CO2_cleaned, "CO2_data_trimmed.csv")
+
+CO2_cleaned <- read_csv("CO2_data_trimmed.csv")
+#Calculate ml CO2 per minute
+#First per second, then times 60
+#USe only values between 1000-2000ppm
+
+#Get time in seconds
+CO2_filtered <- CO2_cleaned %>%
+  filter(CO2 >= 1000, CO2 <= 2000) %>%
+  mutate(TAIR = as.numeric(TAIR)) %>%
+  mutate(AIRPRES = as.numeric(`AIR PRESSURE`)) %>%
+  mutate(
+    EventSec = period_to_seconds(hms(`EVENT TIME`))
+  )
+
+ggplot(subset(CO2_filtered, `EVENT DATE` == "25/05/25"),
+       aes(x = EventSec, y = CO2)) +
+  geom_point()+
+  geom_smooth()+
+  facet_wrap(~ SourceFile, scales = "free_x")
+
 CO2_min <- CO2_filtered %>%
   group_by(`EVENT DATE`, SourceFile) %>%
   summarize(
     TotalRelease = max(CO2) - min(CO2), #How much was released
     TimeSec = max(EventSec) - min(EventSec), #During how many seconds
-    ReleasePerMin = (TotalRelease/TimeSec) * 60,
+    ReleasePerMin = TotalRelease/TimeSec * 60,
     Temp = mean(TAIR),
     Pressure = mean(AIRPRES)
   )
@@ -180,9 +298,6 @@ CO2_min <- CO2_min %>%
     Hour = str_extract(SourceFile, "(?<=_)[0-9]+(?=\\.txt)")
   ) %>%
   mutate(Hour = as.numeric(Hour))  # optional, makes it numeric
-
-CO2_min <- CO2_min %>%
-  filter(ReleasePerMin <= 10000)
 
 #Calculate ppm to ml
 #ml = Volume of chamber in mm * ppm/10^6 * Pressure * 273.15/Temp in Kelvin
@@ -204,6 +319,13 @@ CO2_min <- CO2_min %>%
     mlpermin = 37000 * ReleasePerMin/10^6
   ) 
 
+filtered <- CO2_min %>%
+  filter(Hour >= 13, Hour <= 16)
+
+max(CO2_min$mlpermin)
+min(CO2_min$mlpermin)
+mean(CO2_min$mlpermin)
+
 write.csv(CO2_min, "CO2_per_minute.csv")
 
 #Look at data
@@ -212,6 +334,12 @@ mlpermin_plotraw <- ggplot(CO2_min, aes(x = Hour, y = mlpermin)) +
   theme_bw()+
   xlab("Hour of day")+
   ylab("ml CO2 / min")+
+  geom_smooth(
+    method = "gam",
+    formula = y ~ poly(x, 4),
+    se = FALSE,
+    color = "cornflowerblue"
+  ) +
   scale_x_continuous(limits = c(9,22), n.breaks = 8)+
   theme(axis.title.y = element_text(size=16),
         axis.title.x = element_text(size=16),
